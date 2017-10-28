e2d6a9f7c49ef7d33668656879acaf8303ea0fd2
arya-ayush <arya.ayush15@gmail.com>
2017-10-29 02:45:10 +0530

    Shaking done.

--- app/src/main/java/logicturtle/innovaceraccidentalert/MainActivity.java
+++ app/src/main/java/logicturtle/innovaceraccidentalert/MainActivity.java
@@ -1,12 +1,9 @@
 package logicturtle.innovaceraccidentalert;
 
-import android.content.Intent;
-import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/logicturtle/innovaceraccidentalert/ShakeService.java
@@ -0,0 +1,57 @@
+package logicturtle.innovaceraccidentalert;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.IBinder;
+import android.os.Vibrator;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by user on 29-10-2017.
+ */
+public class ShakeService extends Service implements ShakeListener.OnShakeListener {
+    private ShakeListener mShaker;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    public int check;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+    public void onCreate() {
+        super.onCreate();
+        Log.d("ayush","service created ");
+        this.mSensorManager = ((SensorManager)getSystemService(Context.SENSOR_SERVICE));
+        this.mAccelerometer = this.mSensorManager.getDefaultSensor(1);
+        mShaker = new ShakeListener(this);
+        mShaker.setOnShakeListener(this);
+        Toast.makeText(ShakeService.this, "Service is created!", Toast.LENGTH_LONG).show();
+        Log.d(getPackageName(), "Created the Service!");
+        check=1;
+    }
+    @Override
+    public void onShake() {
+        if(check==1) {
+            Toast.makeText(ShakeService.this, "SHAKEN!", Toast.LENGTH_LONG).show();
+            final Vibrator vib = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            vib.vibrate(500);
+            //sms api call here.............
+        }
+
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+
+    }
+    public void onDestroy(){
+        super.onDestroy();
+        check=0;
+        Log.d(getPackageName(),"Service Destroyed.");
+    }
+}
